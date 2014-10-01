b7e9c55ec43415d6fa8511a0dc4858457a44364b
Amelia Winger-Bearskin <ameliawb@nyu.edu>
2014-10-01 13:46:24 -0400

    ok reset actually resets
    clone works and also changes colors

--- react.js
+++ react.js
@@ -41,16 +41,37 @@ $(".button7").click(function() {
 $( ".box" ).clone().prependTo( ".container" );
 $('#blue').css({"background":"none"});// remove existing
 $('#blue').css({"background":"#662277"});// add new color here.
+$( "#blue" ).click(function() {
+  $( this ).remove();
+});
 $('#red').css({"background":"none"});// remove existing
 $('#red').css({"background":"#3355ff"});// add new color here.
+$( "#red" ).click(function() {
+  $( this ).remove();
+});
 $('#green').css({"background":"none"});// remove existing
-$('#green').css({"background":"#99ffaa"});// add new color here.
+$('#green').css({"background":"#99ffaa"});
+$( "#green" ).click(function() {
+  $( this ).remove();
+});
+
+// add new color here.
  });
 
     /* reset all boxes to the original starting position and style*/
 
     $(".button6").click(function() {
-        $('.box').removeAttr('style'); 
+     /*   $('.box').removeAttr('style'); */
+      $(".container").remove();
+      $("<div></div>").attr('class','container').appendTo('.instruction');
+       $("<div></div>").attr('class','box').appendTo('.container');
+        $("<div></div>").attr('id','red').appendTo('.box');
+        $("<div></div>").attr('id','blue').appendTo('.box');
+        $("<div></div>").attr('id','green').appendTo('.box');
+
+
+
+        
     }); 
         $(".button5").click(function() {
             $(".box").css({ opacity: 0.8 });
