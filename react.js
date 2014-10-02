a83e7e3d1b3c244f8c58a79b86dfb75c879d42af
Amelia Winger-Bearskin <ameliawb@nyu.edu>
2014-10-02 00:06:29 -0400

    lots of changing

--- react.js
+++ react.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    $("#pink").hide();
+    $("#purple").hide();
+    $("#yellow").hide();
     $(".button").click(function() {
         $("#green").animate({
             height: "300px"
@@ -9,8 +12,17 @@ $(document).ready(function() {
         $("#red").animate({
             height: "500px"
         });
+        $("#purple").animate({
+            height: "600px"
+        });
+        $("#yellow").animate({
+            height: "700px"
+        });
+        $("#pink").animate({
+            height: "800px"
+        });
     });
-        $(".button2").click(function() {
+    $(".button2").click(function() {
         $(".box").animate({
             height: '50px',
             width: '300px'
@@ -27,6 +39,15 @@ $(document).ready(function() {
         $("#red").animate({
             width: "700px"
         });
+         $("#purple").animate({
+            width: "800px"
+        });
+        $("#yellow").animate({
+            width: "900px"
+        });
+        $("#pink").animate({
+            width: "1000px"
+        });
     });
     $(".button4").click(function() {
         $(".box").animate({
@@ -36,64 +57,53 @@ $(document).ready(function() {
         /* add after the curly bracket},1000); to set the timing of the animation*/
     });
 
-/*make duplicates for crazy shizzz to happen....*/
-$(".button7").click(function() {
-$( ".box" ).clone().prependTo( ".container" );
-$('#blue').css({"background":"none"});// remove existing
-$('#blue').css({"background":"#662277"});// add new color here.
-$( "#blue" ).click(function() {
-  $( this ).remove();
-});
-$('#red').css({"background":"none"});// remove existing
-$('#red').css({"background":"#3355ff"});// add new color here.
-$( "#red" ).click(function() {
-  $( this ).remove();
-});
-$('#green').css({"background":"none"});// remove existing
-$('#green').css({"background":"#99ffaa"});
-$( "#green" ).click(function() {
-  $( this ).remove();
-});
-
-// add new color here.
- });
-
-    /* reset all boxes to the original starting position and style*/
-
-    $(".button6").click(function() {
-     /*   $('.box').removeAttr('style'); */
-      $(".container").remove();
-      $("<div></div>").attr('class','container').appendTo('.instruction');
-       $("<div></div>").attr('class','box').appendTo('.container');
-        $("<div></div>").attr('id','red').appendTo('.box');
-        $("<div></div>").attr('id','blue').appendTo('.box');
-        $("<div></div>").attr('id','green').appendTo('.box');
-
-
-
-        
-    }); 
-        $(".button5").click(function() {
-            $(".box").css({ opacity: 0.8 });
-            var h = $(window).height();
-            var w = $(window).width();
-            $('.box').each(function() {
-                var originalOffset = $(this).position(),
-                    $this = $(this),
-                    tLeft = w - Math.floor(Math.random() * 900),
-                    tTop = h - Math.floor(Math.random() * 900);
-
-                $(this).animate({
-                    "left": tLeft,
-                    "top": tTop
-                }, 3000, function() {
-                    $this.animate({
-                        "left": originalOffset.left,
-                        "top": originalOffset.top+50
-                    }, 5000);
-                });
-            });
-        });
+    /*make duplicates for crazy shizzz to happen....*/
+    $(".button7").click(function() {
+       $("#pink").show();
+    $("#purple").show();
+    $("#yellow").show();
     });
 
 
+    /* reset all boxes to the original starting position and style*/
+    var wide = 165;
+    var high = 165;
+
+    $(".button6").click(function() {
+       $(".box").width(wide).height(high);
+       $(".box").animate({
+        top: 5,
+        left: 3
+    });
+        $("#pink").hide();
+        $("#yellow").hide();
+        $("#purple").hide();
+    });
+
+
+    /*animation....*/
+    $(".button5").click(function() {
+        $(".box").css({
+            opacity: 0.8
+        });
+        var h = $(window).height();
+        var w = $(window).width();
+        $('.box').each(function() {
+            var originalOffset = $(this).position(),
+                $this = $(this),
+                tLeft = w - Math.floor(Math.random() * 500),
+                tTop = h - Math.floor(Math.random() * 500);
+
+            $(this).animate({
+                "left": tLeft,
+                "top": tTop
+            }, 2000, function() {
+                $this.animate({
+                    "left": originalOffset.left+100,
+                    "top": originalOffset.top -30
+                }, 3000);
+            });
+        });
+
+    });
+});
